44f69d9522d8a089cab07a195bf4d66dc5707056
Garrett Graupmann <garrett_graupmann@intuit.com>
2017-10-12 10:12:34 -0700

    Temporarily commented out Go AST lib - for later use.

---
+++ analyze.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	//github.com/GoASTScanner/gas
+)
+
+func analyzeCode() {
+	// 1. get repo data from Github
+	// 2. determine what code the repo has (can be mutliple types)
+	// 3. for each language, run analysis tool(s) and process output
+	// 4. form output(s) into a comment and post to the PR
+	log.Print("beginning code analysis")
+}
--- handlers.go
+++ handlers.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/bitly/go-simplejson"
 )
 
 // index handles the requests to the main page
@@ -22,11 +25,42 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 // for the specified repo
 func analyze(w http.ResponseWriter, r *http.Request) {
 	log.Print("received analyis request")
-	sendResponse(w, `{"message": "received request to analyze code"}`, http.StatusOK)
 
-	// get repo data from Github
+	// parse the request body and convert to JSON
+	if r.Body == nil {
+		log.Print("received nil request body")
+		sendResponse(w, `{"error": "received empty request body"}`, http.StatusBadRequest)
+		return
+	}
+	bDat, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print("couldn't read byte data from request")
+		sendResponse(w, `{"error": "failed to parse request data; try resending the message"}`, http.StatusInternalServerError)
+		return
+	}
+	r.Body.Close()
+	jsDat, err := simplejson.NewJson(bDat)
+	if err != nil {
+		log.Print("couldn't marshal request byte data to JSON")
+		sendResponse(w, `{"error": "failed to convert request data to JSON; try resending the message"}`, http.StatusInternalServerError)
+		return
+	}
 
-	// determine what code the repo has (can be mutliple types)
+	// check the event type; we only care about newly opened PRs (to avoid double commenting)
+	if jsDat.Get("action").MustString() != "opened" {
+		log.Print("received a message for an event that was not 'opened'; skipping")
+		sendResponse(w, `{"message": "event received was not for PR open; skipping this event"}`, http.StatusOK)
+		return
+	}
 
-	// for each language
+	// check to see if we have permissions to view the PR/code
+	hasPerms := false
+	if !hasPerms && false { // TODO: remove the disabler
+		sendResponse(w, `{"error": "Orion does not have permissions to view the PR or Github repo"}`, http.StatusInternalServerError)
+		return
+	}
+
+	// begin analysis of repo code
+	sendResponse(w, `{"message": "received request to analyze code; beginning analysis"}`, http.StatusOK)
+	analyzeCode()
 }
