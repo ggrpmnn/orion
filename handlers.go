f95933ec962e44b42dc6cc9c8b8e7c8eeaf2f3b2
Garrett Graupmann <garrett_graupmann@intuit.com>
2017-10-24 11:32:38 -0700

    Replaced calls to log.Print with log.Printf for consitency.

--- handlers.go
+++ handlers.go
@@ -11,44 +11,44 @@ import (
 
 // index handles the requests to the main page
 func index(w http.ResponseWriter, r *http.Request) {
-	log.Print("received homepage request from " + r.RemoteAddr)
+	log.Printf("received homepage request from " + r.RemoteAddr)
 	fmt.Fprintln(w, "Orion Homepage")
 }
 
 // notFound handles any requests to unknown resources
 func notFound(w http.ResponseWriter, r *http.Request) {
-	log.Print("received unknown " + r.Method + " request for '" + r.URL.String() + "' from " + r.RemoteAddr)
+	log.Printf("received unknown " + r.Method + " request for '" + r.URL.String() + "' from " + r.RemoteAddr)
 	sendResponse(w, `{"error": "requested resource does not exist"}`, http.StatusNotFound)
 }
 
 // analyze responds immediately and begins the code analysis
 // for the specified repo
 func analyze(w http.ResponseWriter, r *http.Request) {
-	log.Print("received analyis request from " + r.RemoteAddr)
+	log.Printf("received analyis request from " + r.RemoteAddr)
 
 	// parse the request body and convert to JSON
 	if r.Body == nil {
-		log.Print("received nil request body")
+		log.Printf("received nil request body")
 		sendResponse(w, `{"error": "received empty request body"}`, http.StatusBadRequest)
 		return
 	}
 	bytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Print("couldn't read byte data from request")
+		log.Printf("couldn't read byte data from request")
 		sendResponse(w, `{"error": "failed to parse request data; try resending the message"}`, http.StatusInternalServerError)
 		return
 	}
 	r.Body.Close()
 	js, err := simplejson.NewJson(bytes)
 	if err != nil {
-		log.Print("couldn't marshal request byte data to JSON")
+		log.Printf("couldn't marshal request byte data to JSON")
 		sendResponse(w, `{"error": "failed to convert request data to JSON; try resending the message"}`, http.StatusInternalServerError)
 		return
 	}
 
 	// check the event type; we only care about newly opened PRs (to avoid double commenting)
 	if js.Get("action").MustString() != "opened" {
-		log.Print("received a message for an event that was not 'opened'; skipping")
+		log.Printf("received a message for an event that was not 'opened'; skipping")
 		sendResponse(w, `{"message": "event received was not for PR open; skipping this event"}`, http.StatusOK)
 		return
 	}
