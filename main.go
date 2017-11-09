4049611f5cd6246f3cea1646f75487aff62fa63d
Garrett Graupmann <garrett_graupmann@intuit.com>
2017-11-08 16:06:18 -0800

    Removed unnecessary comments.

--- main.go
+++ main.go
@@ -7,19 +7,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Port is the port on which the server will listen
 const port = "8080"
 
 func main() {
-	// create the router
 	router := mux.NewRouter().StrictSlash(true)
 
-	// set up the endpoints
 	router.HandleFunc("/", index)
 	router.HandleFunc("/analyze", analyze).Methods("POST")
 	router.NotFoundHandler = http.HandlerFunc(notFound)
 
-	// start listening for requests
 	log.Print("listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
